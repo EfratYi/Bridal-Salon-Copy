d0e6ddf58ac412ca1651da17750ddcabedf0dbba
EfratYi <144124811+EfratYi@users.noreply.github.com>
2026-09-14 21:27:02 +0300

    fix(startup): correct React import and server start script; replace node-sass with sass

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,4 +1,4 @@
-import { React, createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import LogIn from "./Pages/LogIn";
